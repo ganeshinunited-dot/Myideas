c783f019d8bddfb6196f3ab0507b8f8f8146195a
Ganesh <ganeshmaankarki0@gmail.com>
2026-06-08 10:47:59 +0545

    style: replace privacy link with more link in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ const links = [
   { label: "Home", href: "/", icon: <HomeIcon /> },
   { label: "About", href: "/about", icon: <InfoIcon /> },
   { label: "Search JW", href: "/search-jw", icon: <SearchIcon /> },
-  { label: "Privacy", href: "/privacy", icon: <ShieldIcon /> },
+  { label: "More", href: "#", icon: <MoreIcon /> },
 ];
 
 export default function Navbar() {
@@ -146,8 +146,10 @@ function SearchIcon() {
   );
 }
 
-function ShieldIcon() {
+function MoreIcon() {
   return (
-    <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" /></svg>
+    <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z" />
+    </svg>
   );
 }
